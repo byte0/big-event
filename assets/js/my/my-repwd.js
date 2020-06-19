98b274986792afe364a4fe71618fa11ca24fc680
byte0 <bbcde_1234@163.com>
2020-06-19 15:25:33 +0800

    实现修改密码

--- assets/js/my/my-repwd.js
+++ assets/js/my/my-repwd.js
@@ -1,3 +1,21 @@
 $(function () {
-  console.log('repwd')
+  // 控制表单提交
+  $('#form').submit(function (e) {
+    e.preventDefault()
+    // 获取表单数据
+    var fd = $(this).serialize()
+    // 调用接口修改密码
+    $.ajax({
+      type: 'post',
+      url: 'my/updatepwd',
+      data: fd,
+      success: function (res) {
+        // 更新成功后，提示一下
+        if (res.status === 0) {
+          layer.msg(res.message)
+        }
+      }
+    })
+  
+  })
 })
