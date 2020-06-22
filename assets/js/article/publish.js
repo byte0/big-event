cc2eeee66e3bc4fd8e5679fdeaf864020d71968c
byte0 <bbcde_1234@163.com>
2020-06-22 14:21:41 +0800

    实现下来列表功能

--- assets/js/article/publish.js
+++ assets/js/article/publish.js
@@ -26,4 +26,19 @@ $(function () {
       }
     })
   })
+
+  // 动态获取分类列表数据
+  function loadListData () {
+    $.ajax({
+      type: 'get',
+      url: 'my/article/cates',
+      success: function (res) {
+        // 基于模板引擎渲染列表数据
+        var result = template('list-tpl', res)
+        $('#cate-list').html(result)
+        form.render('select')
+      }
+    })
+  }
+  loadListData()
 })
