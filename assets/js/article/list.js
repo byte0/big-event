801dbd3b88eda710cfa5b2e3f0b1fc8deea4d115
byte0 <bbcde_1234@163.com>
2020-07-04 15:39:53 +0800

    文章列表分页

--- assets/js/article/list.js
+++ assets/js/article/list.js
@@ -1,11 +1,12 @@
 $(function () {
   // 获取表单对象
   var form = layui.form
+  var laypage = layui.laypage
   
   // 当前页码
   var pagenum = 1
   // 每页显示的条数
-  var pagesize = 10
+  var pagesize = 3
 
   // 补零函数
   function addZero(n) {
@@ -52,6 +53,33 @@ $(function () {
         // 把数据填充到模板
         var tags = template('table-tpl', res)
         $('.layui-table tbody').html(tags)
+        // 初始化分页效果
+        laypage.render({
+          // 注意，这里的 articlePage 是 ID，不用加 # 号
+          elem: 'articlePage',
+          // 当前页码
+          curr: pagenum,
+          // 数据总数，从服务端得到
+          count: res.total,
+          // 每页显示的条数
+          limit: pagesize,
+          // 页面切换是触发的动作
+          jump: function (obj, first) {
+            // obj 表示分页的所有参数；first用于判断首次加载
+            // 这里触发时，需要修改当前页码
+            pagenum = obj.curr
+            // 重新加载接口数据
+            if (!first) {
+              // 首次不触发，切换页码时触发
+              loadTableData({
+                // 页码：必须从1开始
+                pagenum: pagenum,
+                // 每页显示多少条数据
+                pagesize: pagesize
+              })
+            }
+          }
+        });
       }
     })
   }
